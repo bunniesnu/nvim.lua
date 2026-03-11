0990ad0b82e4a9ace509520b7a0ddf9367eca8ce
Bunniesnu <bunniesnu@gmail.com>
2026-03-12 01:25:55 +0900

    Add vim-fugitive git add . command key map

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -4,6 +4,7 @@ return {
         "tpope/vim-fugitive",
         config = function()
             mapKey('<leader>gs', ":Git<cr>")
+            mapKey('<leader>ga', ":Git add .<cr>")
             mapKey('<leader>gc', ":Git commit<cr>")
             mapKey('<leader>gp', ":Git push<cr>")
             mapKey('<leader>gl', ":Git pull<cr>")
