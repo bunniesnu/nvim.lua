562112b405bea6d12661b2780970b22ff9e97355
Bunniesnu <bunniesnu@gmail.com>
2026-03-13 15:29:14 +0900

    Add telescope-ui-select plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,32 @@
 local mapKey = require("utils.keyMapper").mapKey
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    {
+        'nvim-telescope/telescope.nvim', version = '*',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            -- optional but recommended
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        },
+        config = function()
+            local builtin = require("telescope.builtin")
+            mapKey('<leader>ff', builtin.find_files)
+            mapKey('<leader>fg', builtin.live_grep)
+            mapKey('<leader>fb', builtin.buffers)
+            mapKey('<leader>fh', builtin.help_tags)
+        end
     },
-    config = function()
-        local builtin = require("telescope.builtin")
-        mapKey('<leader>ff', builtin.find_files)
-        mapKey('<leader>fg', builtin.live_grep)
-        mapKey('<leader>fb', builtin.buffers)
-        mapKey('<leader>fh', builtin.help_tags)
-    end
+    {
+        'nvim-telescope/telescope-ui-select.nvim',
+        config = function()
+            require('telescope').setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+                        }
+                    }
+                }
+            })
+            require("telescope").load_extension("ui-select")
+        end
+    }
 }
